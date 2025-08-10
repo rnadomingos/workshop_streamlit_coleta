10102970c306551001a54c51d960f2d38bd3de8c
Renan Domingos <rna.domingos@hotmail.com>
2025-08-10 09:34:38 -0300

    fix: update url database

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
-DATABASE_URL = f"postgresql://{DB_HOST}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 Base = declarative_base()
 
